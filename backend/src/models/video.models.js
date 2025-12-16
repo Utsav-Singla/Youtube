60077146268e252fe78cb5fd1a8153151183b110
Utsav Singla <utsavsingla2005@gmail.com>
2025-12-16 16:27:24 +0530

    Adding user model video model comment model subscription model

---
+++ backend/src/models/video.models.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+
+const videoSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+      default: '',
+    },
+
+    videoUrl: {
+      type: String,
+      required: true,
+    },
+
+    thumbnailUrl: {
+      type: String,
+      required: true,
+    },
+
+    views: {
+      type: Number,
+      default: 0,
+    },
+
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Video = mongoose.model('Video', videoSchema);
+
+export default Video;
